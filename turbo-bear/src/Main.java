8924598ba2eff8f98d949f0c4369eef544c6a61e
Eiamnacken <Sven2q@hotmail.de>
2014-12-16 15:11:44 +0100

    Einige änderungen Userinterface läuft jetzt paar kommentar hinzugefügt

--- turbo-bear/src/Main.java
+++ turbo-bear/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 	/**
 	 * Prüft ob eine Verbindung zum Datenbank-Server möglich ist
 	 * 
-	 * @return
+	 * @return available Die verfügbarkeit des Servers liefert true wem den so ist 
 	 */
 	public static boolean checkDatabase() {
 
@@ -31,15 +31,15 @@ public class Main {
 	/**
 	 * Prüft, ob die Datenbank-Verbindung noch besteht
 	 * 
-	 * @param con
-	 * @return
+	 * @param con Die Verbidnung zum Server 
+	 * @return connected Wenn verbunden liefert es true sonst false
 	 */
 	// TODO - Funktioniert nicht ?!?
 	public static boolean checkDatabaseConnection(Connection con) {
 		boolean connected = true;
 
 		try {
-			//connected = con.isValid(5);
+			// connected = con.isValid(5);
 		} catch (Exception e) {
 		}
 
@@ -49,17 +49,21 @@ public class Main {
 	/**
 	 * Stellt die Verbindung zur Datenbank her
 	 * 
-	 * @param dbConnection
-	 * @param user - Benutzername
-	 * @param password - Passwort
-	 * @return
+	 * @param dbConnection Die verbidnung zum Server 
+	 * @param user
+	 *            - User zum zufgriff des Servers 
+	 * @param password - Das Passwort des Servers 
+	 * @return 
 	 * @throws Exception
 	 */
-	public static java.sql.Connection connectDatabase(java.sql.Connection dbConnection, String user, String password) throws Exception {
+	public static java.sql.Connection connectDatabase(
+			java.sql.Connection dbConnection, String user, String password)
+			throws Exception {
 
 		Class.forName("com.mysql.jdbc.Driver");
 
-		dbConnection = DriverManager.getConnection("jdbc:mysql://slo.swe.fh-luebeck.de/Fr1c", user, password);
+		dbConnection = DriverManager.getConnection(
+				"jdbc:mysql://slo.swe.fh-luebeck.de/Fr1c", user, password);
 
 		return dbConnection;
 
@@ -67,68 +71,90 @@ public class Main {
 
 	public static void userAuswahl() {
 
+		String zeroString = "";
 		Scanner in = new Scanner(System.in);
-		int choice = -1;
+		int choice = 0;
+		try {
+			do {
+				System.out.println("Bitte waehlen:");
+				System.out.println("0 - Das Programm beenden");
+				System.out
+						.println("1 - Die komplette Tabelle Patient ausgeben.");
+				System.out
+						.println("2 - In die Tabelle Patient einen neuen Patienten einfuegen");
+				choice = in.nextInt();
 
-		while (choice != 0 && choice != 1 && choice != 2) {
-			System.out.println("Bitte waehlen:");
-			System.out.println("0 - Das Programm beenden");
-			System.out.println("1 - Die komplette Tabelle Patient ausgeben.");
-			System.out.println("2 - In die Tabelle Patient einen neuen Patienten einfuegen");
-			choice = in.nextInt();
+				switch (choice) {
 
-			switch (choice) {
+				case 0: {
+					System.out.println("Programm wird beendet.");
+					System.exit(0);
+				}
+					break;
 
-			case 0: {
-				System.out.println("Programm wird beendet.");
-				System.exit(0);
-			}
-				break;
+				case 1: {
+					System.out.println("Methode 1");
+					
 
-			case 1: {
-				System.out.println("Methode 1");
+				}
+					break;
 
-			}
-				break;
+				case 2: {
+					System.out.println("Methode 2");
+					
 
-			case 2: {
-				System.out.println("Methode 2");
+				}
+					break;
 
-			}
-				break;
+				case 21: {
+					System.out.println("You're Stupid!");
+					
 
-			case 21: {
-				System.out.println("You're Stupid!");
+				}
+					break;
 
-			}
-				break;
+				case 42: {
+					try {
+						Desktop.getDesktop()
+								.browse(new URL(
+										"http://www.youtube.com/watch?v=8XfOH1OkrW0#t=84&rel=0&autoplay=1")
+										.toURI());
+					} catch (Exception e) {
+					}
 
-			case 42: {
-				try {
-					Desktop.getDesktop().browse(new URL("http://www.youtube.com/watch?v=8XfOH1OkrW0#t=84&rel=0&autoplay=1").toURI());
-				} catch (Exception e) {
+				}
+					break;
+
+				case 69: {
+					System.out.println("( ¬‿¬)");
+					in.next();
+
+				}
+					break;
+
+				default: {
+					System.out
+							.println("\nBitte geben Sie nur Zahlen zwischen 0 und 2 ein.\n");
 				}
 
-			}
-				break;
+				}
 
-			case 69: {
-				System.out.println("( ¬‿¬)");
-
-			}
-				break;
-
-			default: {
-				System.out.println("\nBitte geben Sie nur Zahlen zwischen 0 und 2 ein.\n");
-			}
-
-			}
-
+			}while(choice!=0);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			in.close();
+			
 		}
 
 	}
 
-	public static void main(String[] args) {
+	/**
+	 * Stellt verbindugn auf und führt dann die weiteren Methoden aus {@link #userAuswahl()}
+	 * @param args Werden nicht ausgewertet 
+	 * @throws SQLException
+	 */
+	public static void main(String[] args) throws SQLException {
 
 		java.sql.Connection dbConnection = null;
 		Scanner in = new Scanner(System.in);
@@ -136,51 +162,59 @@ public class Main {
 		String password = "";
 		boolean connected = false;
 		int counter = 5;
+		try{
+		// Solange keine Verbindung aufgebaut werden konnte
+		while (!connected && counter > 0) {
 
-		 // Solange keine Verbindung aufgebaut werden konnte
-		 while (!connected && counter > 0) {
-		
-		 System.out.println("Bitte authentifizieren Sie sich, um eine Verbindung mit der Datenbank herzustellen.");
-		 System.out.print("User:");
-		 user = in.next();
-		 System.out.print("Passwort:");
-		 password = in.next();
-		 System.out.println();
-		
-		 // Wenn eine Verbindung zur Datenbank möglich ist
-		 if (checkDatabase()) {
-		
-		 try {
-		 dbConnection = connectDatabase(dbConnection, user, password);
-		 System.out.println("Verbindung wurde erfolgreich hergestellt.\n");
-		 connected = true;
-		 } catch (Exception e) {
-		 System.out.println(e.getMessage() + "\n");
-		
-		 if (counter == 0) {
-		 System.out.println("┌∩┐(◣_◢)┌∩┐");
-		 System.out.println("Programm wird beendet.");
-		 System.exit(0);
-		 }
-		 counter++;
-		 }
-		
-		 // Wenn keine Verbindung möglich ist
-		 } else {
-		 counter--;
-		 System.out.println("Keine Verbindung zum Datenbank-Server moeglich.\n");
-		 }
-		
-		 }
+			System.out
+					.println("Bitte authentifizieren Sie sich, um eine Verbindung mit der Datenbank herzustellen.");
+			System.out.print("User:");
+			user = in.next();
+			System.out.print("Passwort:");
+			password = in.next();
+			System.out.println();
 
-		while (true)
+			// Wenn eine Verbindung zur Datenbank möglich ist
+			if (checkDatabase()) {
+
+				try {
+					dbConnection = connectDatabase(dbConnection, user, password);
+					System.out
+							.println("Verbindung wurde erfolgreich hergestellt.\n");
+					connected = true;
+				} catch (Exception e) {
+					System.out.println(e.getMessage() + "\n");
+
+					if (counter == 0) {
+						System.out.println("┌∩┐(◣_◢)┌∩┐");//Nochmal ueberdenken fuer Praeso :D 
+						System.out.println("Programm wird beendet.");
+						System.exit(0);
+					}
+					counter++;
+				}
+
+				// Wenn keine Verbindung möglich ist
+			} else {
+				counter--;
+				System.out.println("Keine Verbindung zum Datenbank-Server moeglich.\n");
+			}
+
+		}
+		
+
+		 
 			userAuswahl();
 
 		// checkDatabaseConnection(dbConnection);
+		}catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
 
 	}
-	
-	public static void tabellePatientAusgeben(Connection dbConnection) throws Exception {
+
+	public static void tabellePatientAusgeben(Connection dbConnection)
+			throws Exception {
 		int anzahl = 0;
 		String ausgabe = "select * from Patient";
 		try {
@@ -192,17 +226,22 @@ public class Main {
 				// Wird zu String gewandelt um die Ausgabe auch von nicht
 				// unterstützten java.util.date formaten auszugeben Beispiel:
 				// 0000-00-00
-				String geburtsdatum = String.valueOf(ergebnis.getLong("Geburtsdatum"));
+				String geburtsdatum = String.valueOf(ergebnis
+						.getLong("Geburtsdatum"));
 				String geschlecht = ergebnis.getString("Geschlecht");
 				int gewicht = ergebnis.getInt("Gewicht");
 				int groesse = ergebnis.getInt("Größe");
 				String telefonNummer = ergebnis.getString("Telefon");
-				String aufnahmeDatum = String.valueOf(ergebnis.getDate("Aufnahmedatum"));
-				String entlassDatum = String.valueOf(ergebnis.getDate("Entlassdatum"));
+				String aufnahmeDatum = String.valueOf(ergebnis
+						.getDate("Aufnahmedatum"));
+				String entlassDatum = String.valueOf(ergebnis
+						.getDate("Entlassdatum"));
 				anzahl++;
 
-				System.out.println("Tupel " + anzahl + ": " + name + "," + vorname + "," + geburtsdatum + "," + geschlecht + "," + gewicht + ","
-						+ groesse + "," + telefonNummer + "," + aufnahmeDatum + "," + entlassDatum);
+				System.out.println("Tupel " + anzahl + ": " + name + ","
+						+ vorname + "," + geburtsdatum + "," + geschlecht + ","
+						+ gewicht + "," + groesse + "," + telefonNummer + ","
+						+ aufnahmeDatum + "," + entlassDatum);
 			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
